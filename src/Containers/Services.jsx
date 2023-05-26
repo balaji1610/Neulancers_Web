38af6e39d195552bfffea311eb5fb8526648f782
balaji1610 <balajiece1610@gmail.com>
2023-05-26 18:57:44 +0530

    Services_page

---
+++ src/Containers/Services.jsx
@@ -0,0 +1,25 @@
+export default function Services() {
+  return (
+    <div>
+      <div className="container text-center">
+        <div className="col-md-12 col-sm-12">
+          <h1>Our Application Development Services</h1>
+
+          <div className="Services_desktop">
+            <h6>
+              App transforms the app development process so you can ship more,
+            </h6>
+            <h6>faster.Depoly to six targets from a single codebase</h6>
+          </div>
+
+          <div className="Services_mobile">
+            <h6>
+              App transforms the app development process so you can ship
+              more,faster.Depoly to six targets from a single codebase
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import ContactUs from "../Containers/ContactUs";
+import Services from "../Containers/Services";
 export default function HomePage() {
   return (
     <div>
       <ContactUs />
+      <Services />
     </div>
   );
 }
