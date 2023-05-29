6aa644f7c0b39e469ef2959ef30aedc9c8c6718b
balaji1610 <balajiece1610@gmail.com>
2023-05-29 20:51:23 +0530

    What we offer

--- src/Mini_Containers/OfferBox.jsx
+++ src/Mini_Containers/OfferBox.jsx
@@ -1,10 +1,10 @@
 export default function OfferBox({ number, title1, title2, imgSrc, alt }) {
   return (
     <div class="d-flex justify-content-around">
-      <div>{number}</div>
-      <div class="align-self-start">
+      <div style={{ marginTop: "24px" }}>{number}</div>
+      <div class="align-self-start" style={{ marginLeft: "22px" }}>
         <div>
-          <h3>
+          <h3 style={{ marginTop: "30px" }}>
             {" "}
             {title1} <br />
             {title2}
@@ -14,7 +14,7 @@ export default function OfferBox({ number, title1, title2, imgSrc, alt }) {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
         </div>
       </div>
-      <div>
+      <div style={{ marginTop: "-28px" }}>
         <img width="67" height="67" src={imgSrc} alt={alt} />
       </div>
     </div>
