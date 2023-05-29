d8a433a37989cdfda25fa00e3c63d1cbd5120bac
balaji1610 <balajiece1610@gmail.com>
2023-05-30 03:17:05 +0530

    development

--- src/Mini_Containers/HeadofDevelopment.jsx
+++ src/Mini_Containers/HeadofDevelopment.jsx
@@ -3,7 +3,13 @@ export default function HeadofDevelopment() {
     <div className="container text-center ">
       <div className="Development_process_first">
         <div className="col-md-12 col-sm-12  ">
-          <h1 style={{ color: "#ffffff" }}>Our Development Process</h1>
+          <h1 style={{ fontWeight: "normal", color: "#ffffff" }}>
+            Our &nbsp;
+            <span style={{ fontWeight: "bold", color: "#ffffff" }}>
+              Development
+            </span>{" "}
+            Process
+          </h1>
         </div>
       </div>
     </div>
