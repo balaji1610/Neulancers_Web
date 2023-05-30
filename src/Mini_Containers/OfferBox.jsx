426816bdba7cde75957b674d217b99e0346524de
balaji1610 <balajiece1610@gmail.com>
2023-05-30 23:46:01 +0530

    whatwe offer styles

--- src/Mini_Containers/OfferBox.jsx
+++ src/Mini_Containers/OfferBox.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 export default function OfferBox({ number, title1, title2, imgSrc, alt }) {
+  const [offerbox, setOfferBox] = useState(false);
+
   return (
-    <div class="d-flex justify-content-around">
+    <div
+      class="d-flex justify-content-around"
+      style={{
+        cursor: "pointer",
+        color: offerbox ? "#ffffff" : "#000000",
+        backgroundColor: offerbox ? "#0288D1 " : "#f4f4f4",
+        height: "201px",
+        borderRadius: "9px",
+      }}
+      onMouseEnter={() => setOfferBox(true)}
+      onMouseLeave={() => setOfferBox(false)}
+    >
       <div style={{ marginTop: "24px" }}>{number}</div>
       <div class="align-self-start" style={{ marginLeft: "22px" }}>
         <div>
--- src/Mini_Containers/OfferCard.jsx
+++ src/Mini_Containers/OfferCard.jsx
@@ -3,9 +3,9 @@ export default function OfferCard({ children }) {
     <div className="row box_align_set">
       {" "}
       <div className="col-md-1 col-sm-1 "></div>
-      <div className="col-md-4 col-sm-4 box_offer_01">{children[0]}</div>
+      <div className="col-md-4 col-sm-4">{children[0]}</div>
       <div className="col-md-1 col-sm-1 box_offer_align"></div>
-      <div className="col-md-4 col-sm-4 box_offer_01"> {children[1]}</div>
+      <div className="col-md-4 col-sm-4 "> {children[1]}</div>
     </div>
   );
 }
